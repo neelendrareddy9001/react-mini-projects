472b103261c2b2cf3bc3a795bd0a553e60c0159b
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-31 21:31:43 +0100

    Add files via upload

---
+++ src/LocalStorage/storage.js
@@ -0,0 +1,15 @@
+import createStore from 'react-persist-store'
+
+const initialState = {
+    user : {
+        name : 'Code with yd'
+    },
+    login : {
+        token : 'xyz'
+    }
+};
+
+const store = createStore(initialState, {storage : 'session', namespace : 'test'});
+
+export const login = store("login");
+export const user = store("user");
