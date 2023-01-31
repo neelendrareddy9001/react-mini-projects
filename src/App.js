dac2600a85191a136e192e02631858043895246e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-31 21:29:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Hello from './Hello';
 import CounterUpPage from './CounterUp/CounterUpPage';
 import './App.css';
 import './App.scss';
+import A from './A'
 
 function App() {
   return (
     <div className="App">
        {/* <Hello/> */}
-       <CounterUpPage/>
+       {/*<CounterUpPage/> */}
+        <A/>
     </div>
   );
 }
