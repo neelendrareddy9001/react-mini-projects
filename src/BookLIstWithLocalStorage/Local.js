6c91a838536a35e453571844aba66be399ce8fab
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-01 01:38:57 +0100

    Add files via upload

---
+++ src/BookLIstWithLocalStorage/Local.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './storea.css'
+
+const Local = () => {
+    return (
+        <div className='wrapper'>
+            <h1>BookList App</h1>
+            <p>And and view your books using local storage</p>
+            <div className='main'>
+                <div className='form-container'>
+                    
+                </div>
+                <div className='view-container'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Local
