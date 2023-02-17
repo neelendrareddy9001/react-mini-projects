b13bb3f10a80ddd1cd2ea040990fc63a190a227f
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-17 23:17:21 +0100

    Add files via upload

---
+++ src/accord/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+import data from './component/data';
+import Question from './component/Question';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Accordition project setup</h2>
+    </div>
+  );
+}
+
+export default App;
