88656740f9c906a5375435ae2e9407426630e36b
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-19 23:27:12 +0100

    Update App.js

--- src/menu/src/App.js
+++ src/menu/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import Menu from './Menu';
+import items from './data';
 import './App.css';
+import Categories from './Categories';
+
+
+const allCategories = ['all', ...new Set((items.map((item)=> item.category)))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const fillterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => 
+      item.category === category);
+      setMenuItems(newItems);
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} fillterItems={fillterItems}/>
+        <Menu items={menuItems}/>
+      </section>
+    </main>
   );
 }
 
